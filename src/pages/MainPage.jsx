8c9dcd2ff355dc4eff1453ecd13e707f63fcef32
Эльз <elzara.freedomsk02@gmail.com>
2026-09-18 12:57:07 +0300

    feat: extend add-pupil form with subject, age and contacts

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,13 +5,32 @@ import Button from '../components/Button.jsx'
 import { addPupil } from '../api/pupils.js'
 function MainPage(){
     const [name, setName ] = useState ('')
+    const [subject, setSubject] = useState('')
+    const [age, setAge] = useState('')
+    const [phone, setPhone] = useState('')
+    const [telegram, setTelegram] = useState('')
+    
     function handleSubmit(e){
     e.preventDefault()
     if (!name.trim()){
         return
     }
-    addPupil(name)
-        .then(() => setName('')) 
+    const pupilData = {
+        name: name,
+        subject: subject,
+        age: Number(age),
+        contacts:{
+            phone: phone,
+            telegram: telegram
+        }
+    }
+    addPupil(pupilData)
+        .then(() => {setName('')
+            setSubject('')
+            setAge('')
+            setPhone('')
+            setTelegram('')
+        }) 
 }
     return(
         <main style={{padding:'20px'}}>     
@@ -21,7 +40,11 @@ function MainPage(){
             <div style = {{}}>
                 <form onSubmit={handleSubmit}>
                     <Button variant="primary" type="submit">Добавить ученика</Button>
-                    <input value={name} onChange={(e) => setName(e.target.value)} />
+                    <input value={name} placeholder = "Имя" onChange={(e) => setName(e.target.value)} />
+                    <input value={subject} placeholder = "Предмет/цель занятий" onChange={(e) => setSubject(e.target.value)} />
+                    <input value={age} placeholder = "Класс/возраст" onChange={(e) => setAge(e.target.value)} />
+                    <input value={phone} placeholder = "Телефон" onChange={(e) => setPhone(e.target.value)} />
+                    <input value={telegram} placeholder = "Телеграм" onChange={(e) => setTelegram(e.target.value)} />
                 </form>
                 <Button variant="secondary">Отмена</Button>
                 <Button variant="danger">Удалить</Button>
